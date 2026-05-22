b1466a5837c8c14932d56be9821c56153f0cc2c2
Nico643-13 <nicperez88@gmail.com>
2026-05-21 20:44:01 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "import.meta.env.VITE_FIREBASE_API_KEY",
-  authDomain: "stl8-cbm-dashboard.firebaseapp.com",
-  projectId: "stl8-cbm-dashboard",
-  storageBucket: "stl8-cbm-dashboard.firebasestorage.app",
-  messagingSenderId: "356283622439",
-  appId: "1:356283622439:web:32ea7e908ffeda58061a02",
-  measurementId: "G-VE9RDS6TJ6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
